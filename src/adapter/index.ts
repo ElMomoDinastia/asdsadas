f689ca70db97f9875d081a7860158c56fd9ac77a
ElMomoDinastia <84684306+ElMomoDinastia@users.noreply.github.com>
2025-12-31 13:03:17 -0300

    Delete src directory

--- src/adapter/index.ts
+++
@@ -1,3 +0,0 @@
-export { IHBRoomAdapter, HBPlayer, RoomConfig, RoomEventHandlers, AnnouncementOptions } from './types';
-export { HBRoomAdapter, createHBRoomAdapter } from './haxball.adapter';
-export { MockHBRoomAdapter, createMockAdapter } from './mock.adapter';
